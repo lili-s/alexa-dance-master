23e415bf7bca1c40b2b1d4c383be661cdb48efdb
lili s <sunlili@hotmail.com>
2019-05-25 11:07:57 -0400

    update select number

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.get('/values/current', async (req, res) => {
 });
 
 app.get('/value/id', async (req, res) => {
-  const dancer= req.params.dancer;
+  const dancer= req.query.dancer;
   const values = await pgClient.query('SELECT * from values where number = $1', [dancer]);
 
   res.send(values.rows);
